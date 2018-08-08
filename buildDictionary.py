cfe15775336dff6c51433ed44186b42df966b489
smandyscom <smandyscom@gmail.com>
2018-08-08 23:39:18 +0800

    word sorted...

--- buildDictionary.py
+++ buildDictionary.py
@@ -3,21 +3,48 @@ import sqlite3
 
 
 db = sqlite3.connect('database.db')
-file_training_data = open('training_label.txt','r')
+currentId = 1
+totalCount = db.execute("select count(*) from labeled_sentences").fetchone()[0]
+wordPattern = r"(\w+)[\s\W]+"
 
-linePattern = r"^([0|1])[\s](\+\+\+\$\+\+\+)[\s](.+)$"
-wordPattern = ""
+aeiouPattern = r"[a|e|i|o|u]\w*"
+bcdfgPattern = r"[b|c|d|f|g]\w*"
+hjklmPattern = r"[h|j|k|l|m]\w*"
+pqrstPattern = r"[p|q|r|s|t]\w*"
 
-while True :
-    line =file_training_data.readline()
-    if not line:
-        break # empty string
+
+while currentId < totalCount :
+    sentence = db.execute("select sentence from labeled_sentences where id=?",(currentId,)).fetchone()[0]
+    currentId = currentId+1
     #disamble
-    match = re.match(linePattern,line)
-    label = match.group(1)
-    sentence = match.group(3)
-    # write into database
-    db.execute("insert into labeled_sentences  values (NULL,?,?)",(label,sentence))
+    groups = re.findall(wordPattern,sentence)
+    
+    if (currentId % 1000) == 0 :
+        print(currentId)
 
-db.commit()
+    for word in groups:
+        #not case sensitive
+        word = word.lower()
+        #aeiou sort
+        if re.match(aeiouPattern,word) :
+            table = "word_dictionary_aeiou"
+        elif re.match(bcdfgPattern,word) :
+            table = "word_dictionary_bcdfg"
+        elif re.match(hjklmPattern,word) :
+            table = "word_dictionary_hjklm"
+        elif re.match(pqrstPattern,word) :
+            table = "word_dictionary_pqrst"
+        else :
+            table = "word_dictionary_else"    
+        
 
+        #query table if existed
+        _id = db.execute('select id from {0} where word=?'.format(table),(word,)).fetchone()
+        if not _id :
+            db.execute('insert into {0} values (NULL,?,1)'.format(table),(word,))
+        else  :
+            counter = db.execute('select counter from {0} where id=?'.format(table),_id).fetchone()[0]
+            counter = counter+1
+            db.execute('update  {0} set counter=? where id=?'.format(table),(counter,_id[0]))
+
+    db.commit()
